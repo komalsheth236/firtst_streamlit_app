a19218d4fd0e40d22f62bce9a16845aae8901fe2
Komal Sheth <komalsheth236@gmail.com>
2022-07-13 13:25:28 +0530

    Update streamlit_app.py
    Added menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+
+streamlit.header("Breakfast Menu")
+streamlit.test("Oatmeal")
+streamlit.test("SMoothie")
+streamlit.test("Egg")
