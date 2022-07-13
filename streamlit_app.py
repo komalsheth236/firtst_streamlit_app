227a95fe8e5697998632891122c6e395a84ec02f
Komal Sheth <komalsheth236@gmail.com>
2022-07-13 13:32:59 +0530

    Update streamlit_app.py
    Added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit
+import pandas 
 
-streamlit.title(" 🍌🥭 My Parents New Healthy Dinner 🥝🍇 ")
+streamlit.title(" My Parents New Healthy Dinner ")
 
 streamlit.header("Breakfast Menu")
 streamlit.text("Oatmeal")
 streamlit.text("SMoothie")
 streamlit.text("Egg")
+
+streamlit.title(" 🍌🥭 Bulid Own SMoothies 🥝🍇 ")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
