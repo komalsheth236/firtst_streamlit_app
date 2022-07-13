9b14348284cfbf66a32424389cca848153887a79
Komal Sheth <komalsheth236@gmail.com>
2022-07-13 13:28:49 +0530

    Update streamlit_app.py
    Tried to add one emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Dinner")
+streamlit.title(" U+1F600	My Parents New Healthy Dinner")
 
 streamlit.header("Breakfast Menu")
 streamlit.text("Oatmeal")
